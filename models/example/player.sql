ea38f1012dfcc9c700a55747737666a8c41a0f01
Simeone Pepe (ICONSULTING) <s.pepe@iconsulting.biz>
2021-08-10 16:27:47 +0200

    table

--- models/example/player.sql
+++ models/example/player.sql
@@ -2,10 +2,11 @@
 
 with play_nba as (
 
-    select position,first_name
+    select position,first_name,team
     from `test-cdp-322216`.google_sheets_sipe.nba_players
-    where first_name='LeBron'
+
 )
 
 select *
 from play_nba
+
