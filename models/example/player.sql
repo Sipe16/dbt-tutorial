b1d711840170203de4841f85924a0cc0c79a07cc
Simeone Pepe (ICONSULTING) <s.pepe@iconsulting.biz>
2021-08-10 16:50:08 +0200

    alter table

--- models/example/player.sql
+++ models/example/player.sql
@@ -3,9 +3,10 @@
 with play_nba as (
 
     select position,first_name,team
-    from `test-cdp-322216`.google_sheets_sipe.nba_players
+    from `test-cdp-322216`.google_sheets_sipe.nba_players;
 
 )
+alter table play_nba add email varchar(100);
 
 select *
 from play_nba
